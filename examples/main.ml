1ac00bac2548b1d3ca643ea4a0af0ebe3d128a08
Thomas Letan <lthms@soap.coffee>
2025-05-31 18:13:57 +0200

    Use HTTPS support in cohttp-connpool example
    * What
    
    This commit demonstrates HTTPS support in the `cohttp-connpool-eio`
    example. It introduces the necessary `tls-eio` and `ca-certs`
    dependencies and integrates them into the connection pool creation.
    
    * Why
    
    The previous example exclusively used HTTP, which is insecure and not
    representative of common production usage for web clients. By adding
    HTTPS, the example now demonstrates how to establish secure connections,
    which is crucial for real-world applications and testing the connection
    pool's ability to handle secure protocols.
    
    * How
    
    New dependencies `tls-eio`, `ca-certs`, and `mirage-crypto-rng.unix`
    were added to the project and example dune files. The `main.ml` was
    updated to include helper functions for `Tls.Config.client` and
    `Tls_eio.client_of_flow`, using `Ca_certs.authenticator` for certificate
    validation. The `Cohttp_connpool_eio.make` function now passes a `Some
    (https ~authenticator)` argument, enabling TLS for the pool. The target
    URI was also changed to an HTTPS endpoint.

--- examples/main.ml
+++ examples/main.ml
@@ -1,15 +1,43 @@
 open Eio
 
+(* -------- *)
+
+(* Borrowed from cohttp-eio example
+   https://github.com/mirage/ocaml-cohttp/blob/main/cohttp-eio/examples/client_tls.ml *)
+
+let authenticator =
+  match Ca_certs.authenticator () with
+  | Ok x -> x
+  | Error (`Msg m) ->
+      Fmt.failwith "Failed to create system store X509 authenticator: %s" m
+
+let https ~authenticator =
+  let tls_config =
+    match Tls.Config.client ~authenticator () with
+    | Error (`Msg msg) -> failwith ("tls configuration problem: " ^ msg)
+    | Ok tls_config -> tls_config
+  in
+  fun uri raw ->
+    let host =
+      Uri.host uri
+      |> Option.map (fun x -> Domain_name.(host_exn (of_string_exn x)))
+    in
+    Tls_eio.client_of_flow ?host tls_config raw
+(* -------- *)
+
 let () =
+  Mirage_crypto_rng_unix.use_default ();
   Eio_main.run @@ fun env ->
   Switch.run ~name:"connection pool" @@ fun sw ->
   let connpool =
-    Cohttp_connpool_eio.make ~https:None ~sw ~net:env#net ~n:10
-      (Uri.of_string "http://google.com")
+    Cohttp_connpool_eio.make
+      ~https:(Some (https ~authenticator))
+      ~sw ~net:env#net ~n:10
+      (Uri.of_string "https://soap.coffee/~lthms")
   in
   let str =
     Switch.run ~name:"request" @@ fun sw ->
-    let _, body = Cohttp_connpool_eio.get connpool ~sw "/" in
+    let _, body = Cohttp_connpool_eio.get connpool ~sw "/posts/posts" in
     Eio.Buf_read.(parse_exn take_all) body ~max_size:max_int
   in
   Format.printf "%s\n%!" str
